6c9dbaa5bd21e12945a16085113529bb1ce44815
ElanyFly <149251217+ElanyFly@users.noreply.github.com>
2023-12-05 21:10:43 +0300

    less02 (#2)
    * less02
    
    * Fixed check 2 halfs equal

---
+++ src/Less02MassT5.java
@@ -0,0 +1,26 @@
+/**
+ * 5. Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета);
+ */
+public class Less02MassT5 {
+
+    public static void main (String[] args){
+
+
+        int[] arr = {73, 15, 2, 5, 16, 9, 4, -3, 8, 79};
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int j : arr) {
+            if (j < min) {
+                min = j;
+            } else if (j > max) {
+                max = j;
+            }
+        }
+
+        System.out.println("min = " + min);
+        System.out.println("max = " + max);
+
+    }
+
+}
