773ba735f72101f4df67d76c4feb1646ba8e62d5
ElanyFly <149251217+ElanyFly@users.noreply.github.com>
2023-12-20 15:58:43 +0300

    calc2-1
    задала кнопки функциональные

--- src/Less06b/Main.java
+++ src/Less06b/Main.java
@@ -8,7 +8,7 @@ public class Main implements ActionListener {
 
     //зададим нужные нам переменные
     JFrame frame;
-    JTextField textField;
+    JTextField textField;                                       //будет содержать наши цифры и результат (окошка ввода)
     JButton[] numberButtons = new JButton[10];
     JButton[] functionButtons = new JButton[8];
     JButton addButton, subButton, mulButton, divButton;         // назвали кнопки операций
@@ -27,9 +27,33 @@ public class Main implements ActionListener {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);       //позволяет закрыть программу.
         frame.setSize(420, 550);
         frame.setLayout(null);
+
+        textField = new JTextField();
+        textField.setBounds(50, 25, 300, 50);   //задали позицию этого элемента
+        textField.setFont(myFont);                                  //задали шрифт в этом поле.
+        textField.setEditable(false);                               //запретили печатать в этом поле. Там будет только вывод
+
+        addButton = new JButton("+");                           // добавили кнопку
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        equButton = new JButton("=");
+        delButton = new JButton("Del");
+        clrButton = new JButton("CE");
+
+        functionButtons[0] = addButton;             //назначили кнопки в массив
+        functionButtons[1] = subButton;
+        functionButtons[2] = mulButton;
+        functionButtons[3] = divButton;
+        functionButtons[4] = decButton;
+        functionButtons[5] = equButton;
+        functionButtons[6] = delButton;
+        functionButtons[7] = clrButton;
+
         
 
-
+        frame.add(textField);                                       //добавили его во фрейм
         frame.setVisible(true);                                     //установили видимость
 
     }
