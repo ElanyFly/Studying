80d4b794cce120f5f6d5b0570d14182aada477b3
ElanyFly <149251217+ElanyFly@users.noreply.github.com>
2023-12-21 14:34:04 +0300

    calc4
    добавили операции

--- src/Less06b/Main.java
+++ src/Less06b/Main.java
@@ -107,5 +107,72 @@ public class Main implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        for (int i = 0; i < 10; i++) {
+            if (e.getSource() == numberButtons[i]){
+                textField.setText(textField.getText().concat(String.valueOf(i)));               // нажимая цифры, они появляются в текстфилде
+            }
+        }
+
+        if (e.getSource() == decButton) {
+            textField.setText(textField.getText().concat("."));
+        }
+
+        if (e.getSource() == addButton) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '+';
+            textField.setText("");
+        }
+
+        if (e.getSource() == subButton) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '-';
+            textField.setText("");
+        }
+
+        if (e.getSource() == mulButton) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '*';
+            textField.setText("");
+        }
+
+        if (e.getSource() == divButton) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '/';
+            textField.setText("");
+        }
+
+        if (e.getSource() == equButton) {
+            num2 = Double.parseDouble(textField.getText());
+
+            switch (operator) {
+                case '+' :
+                    result = num1 + num2;
+                    break;
+                case '-' :
+                    result = num1 - num2;
+                    break;
+                case '*' :
+                    result = num1 * num2;
+                    break;
+                case '/' :
+                    result = num1 / num2;
+                    break;
+            }
+            textField.setText(String.valueOf(result));
+            num1 = result;
+        }
+
+        if (e.getSource() == clrButton) {
+            textField.setText("");
+        }
+        if (e.getSource() == delButton) {                               //луп убирает последнее набранное число.
+            String string = textField.getText();
+            textField.setText("");
+            for (int i = 0; i < string.length()-1; i++) {
+                textField.setText(textField.getText() + string.charAt(i));
+                
+            }
+        }
+
     }
 }
