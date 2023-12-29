820f8d1a3c224f1add3e01589a363381d4553e6a
ElanyFly <149251217+ElanyFly@users.noreply.github.com>
2023-12-29 17:50:58 +0300

    КрестикиИнтерф
    Начало

---
+++ src/Less07TicTacToe/Main.java
@@ -0,0 +1,16 @@
+package Less07TicTacToe;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Main {
+
+    public static void main (String[] args) {
+        GameInterface ticTacToe = new GameInterface();
+        
+    }
+
+
+
+
+}
