af3abeb7063a3018c91a35e25fdf017d182f4019
ElanyFly <149251217+ElanyFly@users.noreply.github.com>
2024-02-27 10:06:43 +0300

    ДЗ-1 - Енамы

--- src/AdvLesson02/Exceptions/Hw01Enum.java
+++ src/AdvLesson02/Exceptions/Hw01Enum.java
@@ -1,5 +1,7 @@
 package AdvLesson02.Exceptions;
 
+import java.util.Scanner;
+
 public class Hw01Enum {
 /*
     Написать метод, на вход в который подаётся енум (что-то про дни недели?)
@@ -10,7 +12,72 @@ public class Hw01Enum {
 */
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите день недели: ");
+        String day = scanner.next();
+
+        if (!day.equals(DayOfWeek.values())) {
+            System.out.println("Введён неверный день.");
+        } else {
+            hoursLeft(DayOfWeek.valueOf(day));
+        }
+
 
     }
-    
+
+    public static void hoursLeft(DayOfWeek dayOfWeek) {
+        switch (dayOfWeek) {
+            case Понедельник: {
+                System.out.println("До конца рабочей недели осталось 40 часов.");
+                break;
+            }
+            case Вторник: {
+                System.out.println("До конца рабочей недели осталось 32 часов.");
+                break;
+            }
+            case Среда: {
+                System.out.println("До конца рабочей недели осталось 24 часов.");
+                break;
+            }
+            case Четверг: {
+                System.out.println("До конца рабочей недели осталось 16 часов.");
+                break;
+            }
+            case Пятница: {
+                System.out.println("До конца рабочей недели осталось 8 часов.");
+                break;
+            }
+            case Суббота:
+            case Воскресенье: {
+                System.out.println("Сегодня выходной.");
+                break;
+            }
+            default: {
+                System.out.println("Введён неправильный день недели.");
+                break;
+            }
+        }
+    }
 }
+
+enum DayOfWeek {
+
+    Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье;
+
+//    Monday("Понедельник", "Пн"),
+//    Tuesday("Вторник", "Вт"),
+//    Wednesday("Среда", "Ср"),
+//    Thursday("Четверг", "Чт"),
+//    Friday("Пятница", "Пт"),
+//    Saturday("Суббота", "Сб"),
+//    Sunday("Воскресенье", "Вс");
+//
+//    private String ru;
+//    private String shortRu;
+//    DayOfWeek(String ru, String shortRu) {
+//        this.ru = ru;
+//        this.shortRu = shortRu;
+//    }
+}
+
