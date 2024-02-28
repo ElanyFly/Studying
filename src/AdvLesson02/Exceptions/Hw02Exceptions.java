88eb4e5ac185e2f1b7ae92517426a43a7d909876
ElanyFly <149251217+ElanyFly@users.noreply.github.com>
2024-02-28 17:32:00 +0300

    Hw 02 Except part1

--- src/AdvLesson02/Exceptions/Hw02Exceptions.java
+++ src/AdvLesson02/Exceptions/Hw02Exceptions.java
@@ -13,11 +13,54 @@ MyArrayDataException – с детализацией, в какой именно
 и MyArrayDataException и вывести результат расчета.
 */
     public static void main(String[] args) {
-        int[][] arrayOne = new int[4][4];
+        String[][] arrayOne = new String[4][4];
+        String[][] arrayTwo = new String[6][7];
+        String[][] arrayThree = new String[4][3];
+
+//        fillArray(arrayOne);
+//        printArray(arrayOne);
+
+//        fillArray(arrayTwo);
+
+        fillArray(arrayThree);
+        printArray(arrayThree);
 
     }
 
-    public static void getArray(int[][] arrayInput) {
-arrayInput[0][1] = 75;
+    public static void fillArray(String[][] arrayInput) {
+//заполнение массива строками 4ч4
+        if (arrayInput.length != 4) {
+            throw new RuntimeException("denied");
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (arrayInput[i].length != 4) {
+                throw new RuntimeException("denied");
+            }
+        }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                arrayInput[i][j] = String.valueOf(i);
+            }
+        }
+        arrayInput[3][3] = "Hello!";
+
+    }
+
+    public static void printArray(String[][] arrayInput) {
+//        for (int i = 0; i <= 4; i++) {
+//            System.out.print(i + "\t");
+//        }
+        System.out.println();
+        for (int i = 0; i < 4; i++) {
+//            System.out.print((i + 1) + "\t");
+            for (int j = 0; j < 4; j++) {
+                System.out.print(arrayInput[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 }
+
+
